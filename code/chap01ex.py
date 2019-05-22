921ec73f451424d43bf311dbd1bf666ec3988101
Monish <monish.khambhati@gmail.com>
2019-05-21 22:38:04 -0700

    Chapter 1 solutions completed

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -13,14 +13,46 @@ import sys
 import nsfg
 import thinkstats2
 
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz',
+                nrows=None):
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+    CleanFemResp(df)
+    return df
+
+def CleanFemResp(df):
+    pass
+
+def ValidatePregnum(resp):
+
+    # read the pregnancy frame
+    preg = nsfg.ReadFemPreg()
+
+    # make the map from caseid to list of pregnancy indices
+    preg_map = nsfg.MakePregMap(preg)
+    
+    # iterate through the respondent pregnum series
+    for index, pregnum in resp.pregnum.items():
+        caseid = resp.caseid[index]
+        indices = preg_map[caseid]
+
+        # check that pregnum from the respondent file equals
+        # the number of records in the pregnancy file
+        if len(indices) != pregnum:
+            print(caseid, len(indices), pregnum)
+            return False
+
+    return True
 
 def main(script):
-    """Tests the functions in this module.
 
-    script: string script name
-    """
+    resp = ReadFemResp()
+
+    assert(len(resp) == 7643)
+    assert(resp.pregnum.value_counts()[1] == 1267)
+    assert(ValidatePregnum(resp))
+
     print('%s: All tests passed.' % script)
-
-
 if __name__ == '__main__':
     main(*sys.argv)

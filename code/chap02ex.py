7c2e21f709a209f954fd3c785ad3dcf48f1ca4d2
Monish <monish.khambhati@gmail.com>
2019-05-22 23:36:55 -0700

    Chapter 2 solutions completed

--- code/chap02ex.py
+++ code/chap02ex.py
@@ -15,23 +15,39 @@ import thinkstats2
 
 
 def Mode(hist):
-    """Returns the value with the highest frequency.
-
-    hist: Hist object
-
-    returns: value from Hist
-    """
-    return 0
+    p, x = max([(p, x) for x, p in hist.Items()])
+    return x
+    
 
 
 def AllModes(hist):
-    """Returns value-freq pairs in decreasing order of frequency.
+    return sorted(hist.Items(), key=itemgetter(1), reverse=True)
 
-    hist: Hist object
+def WeightDifference(live, firsts, others):
 
-    returns: iterator of value-freq pairs
-    """
-    return []
+    mean0 = live.totalwgt_lb.mean()
+    mean1 = firsts.totalwgt_lb.mean()
+    mean2 = others.totalwgt_lb.mean()
+
+    var1 = firsts.totalwgt_lb.var()
+    var2 = others.totalwgt_lb.var()
+
+    print('Mean')
+    print('First babies', mean1)
+    print('Others', mean2)
+
+    print('Variance')
+    print('First babies', var1)
+    print('Others', var2)
+
+    print('Difference in lbs', mean1 - mean2)
+    print('Difference in oz', (mean1 - mean2) * 16)
+
+    print('Difference relative to mean (%age points)', 
+          (mean1 - mean2) / mean0 * 100)
+
+    d = thinkstats2.CohenEffectSize(firsts.totalwgt_lb, others.totalwgt_lb)
+    print('Cohen d', d)
 
 
 def main(script):
